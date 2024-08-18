6a629268f5b847f1cbf04420fdac14f111f4d084
mnasirayub <nasir138g@gmail.com>
2024-08-18 18:10:47 +0500

    chakra ui added

---
+++ src/components/SwitchDarkModeToggle.tsx
@@ -0,0 +1,13 @@
+import { Switch } from "@chakra-ui/react"
+
+
+const SwitchDarkModeToggle = () => {
+
+    
+
+  return <>
+  <Switch isChecked colorScheme="green" />
+  </>
+}
+
+export default SwitchDarkModeToggle
